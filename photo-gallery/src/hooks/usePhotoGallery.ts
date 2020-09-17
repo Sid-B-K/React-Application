dbfd9072eb07265ec3245fc5ae5b7eb8589a09d3
Sidharth Kamtam <sidharthkamtam@Sidharths-Air.home>
2020-09-17 10:01:43 +0100

    Edited Tab2.tsx and hooks/usePhotoGallery

--- photo-gallery/src/hooks/usePhotoGallery.ts
+++ photo-gallery/src/hooks/usePhotoGallery.ts
@@ -8,18 +8,30 @@ import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirec
 export function usePhotoGallery() {
 
     const { getPhoto } = useCamera();
-  
-    const takePhoto = async () => {
-      const cameraPhoto = await getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100
-      });
-    };
-  
-    return {
-      takePhoto
-    };
-  }
 
-  export {};
+    const [photos, setPhotos] = useState<Photo[]>([]);
+
+    const takePhoto = async () => {
+        const cameraPhoto = await getPhoto({
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            quality: 100
+        });
+        const fileName = new Date().getTime() + '.jpeg';
+        const newPhotos = [{
+            filepath: fileName,
+            webviewPath: cameraPhoto.webPath
+        }, ...photos];
+        setPhotos(newPhotos)
+    };
+
+    return {
+        photos,
+        takePhoto
+    };
+}
+
+export interface Photo {
+    filepath: string;
+    webviewPath?: string;
+}
--- photo-gallery/src/pages/Tab2.tsx
+++ photo-gallery/src/pages/Tab2.tsx
@@ -5,12 +5,11 @@ import {
   IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
   IonCol, IonImg, IonActionSheet
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
-  const { takePhoto } = usePhotoGallery();
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -19,12 +18,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
